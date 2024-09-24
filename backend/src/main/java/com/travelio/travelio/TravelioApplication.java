5b9876bfa50609dad808fd1b6a951f5c59ae7044
saielhaq <119542836+saielhaq@users.noreply.github.com>
2024-09-24 14:35:58 +0100

    Removed Test Data

--- backend/src/main/java/com/travelio/travelio/TravelioApplication.java
+++ backend/src/main/java/com/travelio/travelio/TravelioApplication.java
@@ -1,31 +1,13 @@
 package com.travelio.travelio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.travelio.travelio.models.Role;
-import com.travelio.travelio.models.User;
-import com.travelio.travelio.repositories.UserRepository;
 
 @SpringBootApplication
-public class TravelioApplication implements CommandLineRunner {
-
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class TravelioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelioApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		userRepository.save(
-				new User("Saad Sai El Haq", "saielhaqs@gmail.com", passwordEncoder.encode("password"), Role.ADMIN));
 	}
 
 }
